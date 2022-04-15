7dc331541d83acbc7e3519c231f2e66d164d5403
leogm360 <leogm360@gmail.com>
2022-04-15 00:13:35 -0300

    feat(pages): Create Landing Page

---
+++ src/pages/LadingPage/index.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/react";
+
+const LadingPage = () => {
+  return (
+    <Flex
+      as="main"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    ></Flex>
+  );
+};
+
+export default LadingPage;
